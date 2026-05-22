70191c0fdadcbccaca09816d47b3e81c4903d289
Afia Zahin <afiazearly@gmail.com>
2026-05-22 20:37:43 +0600

    add 50A solution

---
+++ 50A_Domino_piling.cpp
@@ -0,0 +1,12 @@
+/*
+375656998	May/22/2026 20:33UTC+6	Early_hammie	A - Domino piling	C++23 (GCC 14-64, msys2)	Accepted	92 ms	100 KB*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int M, N;
+    cin >> M >> N;
+    int result = (M * N) / 2;
+    cout << result << endl;
+}
