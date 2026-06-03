ae584d9d8523c067a058289c0ce64e93ec6ff5ff
Afia Zahin <afiazearly@gmail.com>
2026-06-04 00:16:09 +0600

    add 381A solve

---
+++ 381A_Sereja_and_Dima.cpp
@@ -0,0 +1,49 @@
+/*
+377201672	Jun/03/2026 23:59UTC+6	Early_hammie	A - Sereja and Dima	C++23 (GCC 14-64, msys2)	Accepted	46 ms	0 KB*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int SreejaScore = 0;
+    int DimaScore = 0;
+    vector<int> cards(n);
+    for (int i = 0; i < n; i++)
+
+    {
+        cin >> cards[i];
+    }
+    bool sreeja = true;
+    for (int i = 0; i < n; i++)
+
+    {
+        int score;
+        if (cards.front() > cards.back())
+        {
+            score = cards.front();
+
+            cards.erase(cards.begin());
+        }
+        else
+        {
+            score = cards.back();
+            cards.pop_back();
+        }
+
+        if (sreeja)
+        {
+            SreejaScore += score;
+        }
+        else
+        {
+            DimaScore += score;
+        }
+    }
+
+    cout << SreejaScore << " " << DimaScore << endl;
+}
