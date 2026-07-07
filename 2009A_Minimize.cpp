1097ea647209965708034b593dfab24e8e697ad5
Afia Zahin <afiazearly@gmail.com>
2026-07-07 20:31:08 +0600

    add 2009A solution

---
+++ 2009A_Minimize.cpp
@@ -0,0 +1,17 @@
+/*381616880	Jul/07/2026 20:28UTC+6	Early_hammie	A - Minimize!	C++23 (GCC 14-64, msys2)	Accepted	31 ms	0 KB*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        int dif = (b - a);
+
+        cout << dif << endl;
+    }
+}
